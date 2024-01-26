5a4251003def86b95c6bbece518e73207c981e25
HuaHuaChiChi <ksksks102@naver.com>
2024-01-26 16:42:00 +0900

    "잘못 수정됨,,"

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,6 @@ function App() {
     </li>
   ));
 
-  const ageFilter = e.target.age.value;
-
-  const handleAge = () => {
-    
-  }
 
   return (
     <div>
@@ -40,7 +35,10 @@ function App() {
       다음 나이 이상의 학생목록만 출력해요 :{" "}
       {/* TODO: input에 입력된 값(숫자) 이상의 나이를 가진 학생들만 출력하세요. */}
       <input type="number" name="age"
-      onChange={handleAge}
+      onChange={(e) => {
+        setMinAge(e.target.value)
+      }}
+      value={minAge}
       /> 살 이상
       <ul>{studentList}</ul>
     </div>
